f85f64b0f0a6a23705bca1b50f2f5964a1437df6
Flyn <86884406+peyeum@users.noreply.github.com>
2021-09-06 02:55:32 +0700

    Delete el.js

--- projects/todoList/scripts/el.js
+++
@@ -1,9 +0,0 @@
-// element selector function using query selector
-// select element and return as an element or a node list
-export default function el(target) {
-  // check whether the target is a nodelist
-  return document.querySelectorAll(target).length !== undefined &&
-    document.querySelectorAll(target).length > 1
-    ? document.querySelectorAll(target)
-    : document.querySelector(target);
-}
